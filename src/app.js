9ba7924c75aff1755364f689255bd0a1428c3b1d
NemethAngela <nemeth@gmail.com>
2023-03-03 10:22:35 +0100

    Kezdés

---
+++ src/app.js
@@ -0,0 +1,18 @@
+/*
+* File: app.js
+* Author: Németh Angéla
+* Copyright: 2023, Németh Angéla
+* Group: Szoft 1-2 E
+* Date: 2023-03-03
+* Github: https://github.com/NemethAngela/
+* Licenc: GNU GPL
+*/
+
+var bikelist = [    
+    {name: 'cassis', wheel: 28, using: 'offroad', price: 557900},
+    {name: 'Alboin 900', wheel: 28, using: 'trekking', price: 519900},
+    {name: 'Asgard', wheel: 29, using: 'technikás utak', price: 519900},
+    {name: 'Ruga', wheel: 29, using: 'hegyi', price: 372900},
+    {name: 'Reptila', wheel: 28, using: 'városi', price: 308900},
+    {name: 'Sirmium', wheel: 29, using: 'hegyi', price: 264900},
+];
